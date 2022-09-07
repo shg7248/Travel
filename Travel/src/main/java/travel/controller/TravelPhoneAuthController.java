b86bd984db695862ab2ea777c2ef5865f4fedee9
Song <shg7248@gmail.com>
2022-09-07 12:29:23 +0900

    phone, redis modify

--- Travel/src/main/java/travel/controller/TravelPhoneAuthController.java
+++ Travel/src/main/java/travel/controller/TravelPhoneAuthController.java
@@ -29,13 +29,18 @@ public class TravelPhoneAuthController {
 		String api_key = "NCSWLQNCGD42Q5DB";
 		String api_secret = "KYXPY42RJNJQXYPBUC6ELRIX8YXHAK3F";
 		
+		// 랜던번호 생성 (6자리)
+		int rannum = (int)(Math.random() * 900000) + 100000;
+		String rannum2 = String.valueOf(rannum);
+		
+		
 		Message coolsms = new Message(api_key, api_secret);
 		
 		HashMap<String, String> set = new HashMap<String, String>();
 	    set.put("to", "01077437248"); // 수신번호
 
 	    set.put("from", phone); // 발신번호
-	    set.put("text", "테스트 문자"); // 문자내용
+	    set.put("text", "인증번호는 " + rannum2 + "입니다."); // 문자내용
 	    set.put("type", "sms"); // 문자 타입
 	    
 	    coolsms.send(set); // 보내기&전송결과받기
