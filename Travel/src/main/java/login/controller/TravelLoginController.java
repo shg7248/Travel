23813953c10fb03587b61edaf35d1277d45d8150
admin <admin@DESKTOP-VFJPLPR>
2022-09-15 11:29:30 +0900

    20220915 전진환 login옮기기

---
+++ Travel/src/main/java/login/controller/TravelLoginController.java
@@ -0,0 +1,150 @@
+package login.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import login.model.TravelUserDao;
+import login.model.TravelCompanyBean;
+import login.model.TravelCompanyDao;
+import login.model.TravelUserBean;
+
+@Controller
+public class TravelLoginController {
+	private final String command = "userLoginForm.log";
+	private String getPage = "userLoginForm";
+	
+	private final String command2 = "ownerLoginForm.log";
+	private String getPage2 = "ownerLoginForm";
+	
+	@Autowired
+	TravelUserDao tudao;
+	@Autowired
+	TravelCompanyDao tcdao;
+	
+	//selectLogin.log > selectLogin.jsp
+	@RequestMapping(method = RequestMethod.GET,value = "selectLogin.log")
+	public String selectLogin() {
+		
+		return "selectLogin";
+	}
+	//userLoginForm.log > userLoginForm.jsp
+	@RequestMapping(method = RequestMethod.GET,value = command)
+	public String userLogin() {
+		
+		return getPage;
+	}
+	
+	//userRegisterEnd.jsp > main.jsp
+	@RequestMapping(method = RequestMethod.POST,value = command)
+	public String userLogin2(TravelUserBean tubean,
+			HttpServletResponse response,
+			HttpSession session) throws IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		//입력한 이메일과 비밀번호
+		System.out.println("email:" + tubean.getEmail());
+		System.out.println("password:" + tubean.getPwd());
+		PrintWriter writer = response.getWriter();
+
+		//한계정 로그인 정보
+		TravelUserBean login = tudao.getMember(tubean.getEmail());
+		
+		if(login == null) {
+			System.out.println("가입하지 않은 회원");
+
+				writer.println("<script type='text/javascript'>");
+				writer.println("alert('가입하지 않은 회원입니다'); ");
+				writer.println("</script>");
+				writer.flush();
+		}
+		else {// 일치하는 아이디 존재
+			System.out.println("login.getPwd(): " + login.getPwd());
+			if(tubean.getPwd().equals(login.getPwd())) {
+				System.out.println("가입한 회원");
+				session.setAttribute("loginInfo", login);
+			
+				//로그인성공시 이동할 위치
+			return "redirect:/main.log";
+				
+			}
+			else { // 비번이 일치하지 않을 때
+				writer.println("<script type='text/javascript'>");
+				writer.println("alert('비번이 일치하지 않습니다.'); ");
+				writer.println("</script>");
+				writer.flush();
+				return getPage;
+			}
+		}
+		return getPage;
+		
+	}
+	
+	//ownerLoginForm.log > ownerLoginForm.jsp
+		@RequestMapping(method = RequestMethod.GET,value = command2)
+		public String ownerLogin() {
+			
+			return getPage2;
+		}
+		
+		//ownerRegisterEnd.jsp > main.jsp
+		@RequestMapping(method = RequestMethod.POST,value = command2)
+		public String ownerLogin(TravelCompanyBean tcbean,
+				HttpServletResponse response,
+				HttpSession session) throws IOException {
+			response.setContentType("text/html; charset=UTF-8");
+			//입력한 이메일과 비밀번호
+			System.out.println("email:" + tcbean.getEmail());
+			System.out.println("password:" + tcbean.getPwd());
+			PrintWriter writer = response.getWriter();
+
+			//한계정 로그인 정보
+			TravelCompanyBean login = tcdao.getMember(tcbean.getEmail());
+			
+			if(login == null) {
+				System.out.println("가입하지 않은 회원");
+
+					writer.println("<script type='text/javascript'>");
+					writer.println("alert('가입하지 않은 회원입니다'); ");
+					writer.println("</script>");
+					writer.flush();
+			}
+			else {// 일치하는 아이디 존재
+				System.out.println("login.getPwd(): " + login.getPwd());
+				if(tcbean.getPwd().equals(login.getPwd())) {
+					//가입승인이 안낫을때.
+					if(tcbean.getApproyn()==0) {
+						System.out.println("가입승인이 안난 회원");
+
+						writer.println("<script type='text/javascript'>");
+						writer.println("alert('가입승인이 나지않았습니다.'); ");
+						writer.println("</script>");
+						writer.flush();
+					}else {
+						System.out.println("가입한 회원");
+						session.setAttribute("loginInfo", login);
+					
+						//로그인성공시 이동할 위치
+					return "redirect:/main.log";
+					}
+					
+				}
+				else { // 비번이 일치하지 않을 때
+					writer.println("<script type='text/javascript'>");
+					writer.println("alert('비번이 일치하지 않습니다.'); ");
+					writer.println("</script>");
+					writer.flush();
+					return getPage2;
+				}
+			}
+			return getPage2;
+		}
+}
---
+++ Travel/src/main/java/login/controller/TravelRegisterController.java
@@ -0,0 +1,171 @@
+package login.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import login.model.TravelUserDao;
+import login.model.TravelCompanyBean;
+import login.model.TravelCompanyDao;
+import login.model.TravelUserBean;
+
+@Controller
+public class TravelRegisterController {
+	//user
+	private final String command = "/userRegisterForm.log";
+	private String getPage = "/userRegisterForm";
+	private String gotoPage = "/userRegisterEnd";
+	
+	//company
+	private final String command2 = "ownerRegisterForm.log";
+	private String getPage2 = "/ownerRegisterForm";
+	private String gotoPage2 = "/ownerRegisterEnd";
+	
+	private final String command3 = "/ajax.log";
+	private final String command4 = "/ajax2.log";
+	@Autowired
+	TravelUserDao tudao;
+	@Autowired
+	TravelCompanyDao tcdao;
+	@Autowired
+	ServletContext servletContext;
+	
+	//phoneAuthForm.jsp > userRegisterForm.jsp
+	@RequestMapping(method = RequestMethod.GET,value = command)
+	public ModelAndView userRegister(@RequestParam(value = "phone") String phone) {
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(getPage);
+		mav.addObject("phone", phone);
+		System.out.println("phone"+ phone);
+		
+		return mav;
+	}
+
+	//userRegisterForm.jsp > userRegisterEnd.jsp
+	@RequestMapping(method = RequestMethod.POST,value = command)
+	public ModelAndView userRegister(@ModelAttribute("tubean") @Valid TravelUserBean tubean,BindingResult br,
+			@RequestParam("phone") String phone
+			) {
+		
+		ModelAndView mav = new ModelAndView();
+		if(br.hasErrors()){
+			mav.setViewName(getPage);
+			return mav;
+		}
+		//받아온 phone 넣기
+		tubean.setPhone(phone);
+		//user회원가입
+		tudao.insert(tubean);
+		
+		mav.setViewName(gotoPage);
+
+		return mav;
+	}
+
+	//phoneAuthForm2.jsp > ownerRegisterForm.jsp
+	@RequestMapping(method = RequestMethod.GET,value = command2)
+	public ModelAndView ownerRegister(@RequestParam(value = "phone") String phone) {
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(getPage2);
+		mav.addObject("phone", phone);
+		System.out.println("owner phone"+ phone);
+		
+		return mav;
+	}
+
+	//ownerRegisterForm.jsp > ownerRegisterEnd.jsp
+	@RequestMapping(method = RequestMethod.POST,value = command2)
+	public ModelAndView ownerRegister(@ModelAttribute("tcbean") @Valid TravelCompanyBean tcbean,BindingResult br) throws IllegalStateException, IOException {
+		ModelAndView mav = new ModelAndView();
+		
+		//파일명과 확장자
+		System.out.println("img:" + tcbean.getImage());
+		//멀티객체
+		System.out.println("upload:" + tcbean.getUpload());
+		
+		//MultipartRequest multi =new MultipartRequest(request,uploads,5*1024*1024,"UTF-8",new DefaultFileRenamePolicy());
+		
+		if(br.hasErrors()){
+			System.out.println("err");
+			mav.setViewName(getPage2);
+			return mav;
+		}
+		
+		//파일의 저장경로
+		String uploadPath = servletContext.getRealPath("/resources/company");
+		System.out.println("uploadPath:"+uploadPath);
+		
+		//파일객체
+		File file = new File(uploadPath+"/"+ tcbean.getImage());
+		
+		//파일 저장
+		tcbean.getUpload().transferTo(file);
+		
+		//user회원가입
+		tcdao.insert(tcbean);
+		
+		mav.setViewName(gotoPage2);
+
+		return mav;
+	}
+	
+	
+	//ajax
+	@ResponseBody
+	@RequestMapping(value = command3 ,method = RequestMethod.POST)
+	public String ajaxUser(String email) {
+		System.out.println("email"+email);
+		int cnt = tudao.emailCheck(email);
+		if(email == "") {
+			return "null";
+		}else if(cnt > 0 ) {
+			return "fail";
+		}else {
+			return "ok";
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = command4 ,method = RequestMethod.POST)
+	public String ajaxCompany(String email) {
+		System.out.println("email"+email);
+		int cnt = tcdao.emailCheck(email);
+		if(email == "") {
+			return "null";
+		}else if(cnt > 0 ) {
+			return "fail";
+		}else {
+			return "ok";
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/ajax3.log" ,method = RequestMethod.POST)
+	public String ajaxCompanyCnum(String cnum) {
+		System.out.println("cnum"+cnum);
+		int cnt = tcdao.cnumCheck(cnum);
+		if(cnum == "") {
+			return "null";
+		}else if(cnt > 0 ) {
+			return "fail";
+		}else {
+			return "ok";
+		}
+	}
+	
+}
