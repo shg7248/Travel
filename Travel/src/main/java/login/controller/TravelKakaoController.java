f61c45ac9c684965d611d48959c3801c0f35fef8
admin <admin@DESKTOP-VFJPLPR>
2022-09-19 20:41:13 +0900

    20220919 전진환 kakaoLogin완성,나머지 bugfix

---
+++ Travel/src/main/java/login/controller/TravelKakaoController.java
@@ -0,0 +1,71 @@
+package login.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import login.model.TravelUserBean;
+import login.model.TravelUserDao;
+@Controller
+public class TravelKakaoController {
+	private final String command = "kakaoLogin.log";
+	private String getPage = "userLoginForm";
+	@Autowired
+	TravelUserDao tudao;
+
+	@RequestMapping(value = command,method = RequestMethod.GET)
+	public ModelAndView kakaoLogin(@RequestParam String email,HttpSession session,
+			HttpServletResponse response) throws IOException {
+		//location.href='kakaoRegister.log?email='+response.kakao_account.email;
+		ModelAndView mav = new ModelAndView();
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter writer = response.getWriter();
+		
+		//이메일과 플렛폼 체크
+		int cnt = tudao.emailCheck(email);
+		//이메일이 있으면
+		if (cnt == 1) {
+			//플렛폼체크
+			TravelUserBean login = tudao.getMember(email);
+			if (login.getFlatform().equalsIgnoreCase("kakao")) {
+				//카카오로 로그인
+				System.out.println("가입한 회원");
+				session.setAttribute("userInfo", login);
+				session.setAttribute("mnum", login.getMnum());
+				session.setAttribute("email", login.getEmail());
+
+				//로그인성공시 이동할 위치
+				mav.setViewName("redirect:/");
+				return mav;
+			}else {
+				//다른 플렛폼으로 로그인 하세요.
+				System.out.println("가입한 회원 다른플렛폼");
+				writer.println("<script type='text/javascript'>");
+				writer.println("alert('"+login.getFlatform()+"에 가입된 계정입니다.'); ");
+				writer.println("</script>");
+				writer.flush();
+			}
+
+		}else {//이메일이 없으면
+			//카카오로 회원가입
+			
+			mav.addObject("email", email);
+			mav.setViewName("redirect:/phoneAuthForm.log?email="+email+"&flatform=kakao");
+			return mav;
+		}
+		
+		mav.setViewName(getPage);
+		
+		return mav;
+	}
+}
--- Travel/src/main/webapp/WEB-INF/travel/login/userLoginForm.jsp
+++ Travel/src/main/webapp/WEB-INF/travel/login/userLoginForm.jsp
@@ -22,7 +22,68 @@ function checkAll(){
 	}
 }
 </script>    
+<script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+<script type="text/javascript">
+	//2a2af0084ddd5b6b3167da014e30faa3
+	Kakao.init('2a2af0084ddd5b6b3167da014e30faa3');
 
+	// SDK 초기화 여부를 판단합니다.
+	console.log(Kakao.isInitialized());
+
+	function kakaoLogin() {
+		//로그인(이메일받음)
+		Kakao.Auth.login({
+			scope : 'account_email',
+
+			success : function(response) {
+				console.log(response);
+				
+				//사용자 정보 가져오기
+				Kakao.API.request({
+					  url: '/v2/user/me',
+					  data: {
+					    property_keys: ['kakao_account.email']
+					  },
+					  success: function(response) {
+					    console.log(response);
+					    //kakaoLogin.log
+					    location.href='kakaoLogin.log?email='+response.kakao_account.email;
+					 	
+					  },
+					  fail: function(error) {
+					    console.log(error);
+					  }
+					});
+			},
+			fail : function(error) {
+				console.log(error);
+			},
+		});
+	}
+
+	function kakaoLogout() {
+		//토큰확인
+		if (!Kakao.Auth.getAccessToken()) {
+			console.log('Not logged in.');
+			alert("로그인 정보가 없습니다.");
+			return;
+		}
+		//로그아웃
+		Kakao.Auth.logout(function() {
+			console.log(Kakao.Auth.getAccessToken());
+		});
+		//연결끊기
+		Kakao.API.request({
+			url : '/v1/user/unlink',
+			success : function(response) {
+				console.log(response);
+			},
+			fail : function(error) {
+				console.log(error);
+			}
+		});
+	}
+</script>
 userLoginForm.jsp
 <div class="all">
 <form method="post" name="f" action="userLoginForm.log" >
@@ -43,4 +104,10 @@ userLoginForm.jsp
 |
 <a href="phoneAuthForm.log">회원가입</a>
 </div>
+
+<a href="javascript:kakaoLogin();"> <img alt=""
+		src="https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_medium_narrow.png">
+	</a>
+	<Br>
+	<a href="javascript:kakaoLogout();"> 로그아웃 </a><Br>
 </div>
